b037157ea10ba789ff9f69509c35b809be3e50f0
pworden <92079307+pworden@users.noreply.github.com>
2023-10-11 12:43:01 +1100

    Add files via upload

--- cogClassifier.sh
+++ cogClassifier.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+#PBS -l ncpus=16
+#PBS -l mem=64GB
+#PBS -l walltime=48:00:00
+
 # Start the cogclassifier conda environment
 source /shared/homes/118623/miniconda3/etc/profile.d/conda.sh # Path to conda
 conda activate cogclassifier
 
 # ------------------------------------------------------------------------------
 # --------------------------------- ALL INPUTS ---------------------------------
-queryProteinFasta="/shared/homes/118623/bee_project/cogclassifier/scoaryUniprot.faa"
-outputDir="/shared/homes/118623/bee_project/cogclassifier/P_larvae_10-10-23"
+queryProteinFasta="/shared/homes/118623/bee_project/cogclassifier/scoary.faa"
+outputDir="/shared/homes/118623/bee_project/cogclassifier/COG_out"
 # -------------------------------- End All Inputs ------------------------------
 # ------------------------------------------------------------------------------
 
